eab68cb81f2e4b88f3bff3d6cc9677ae31064732
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2021-01-17 08:47:01 +0200

    Update shape.h

--- Encapsulation/shape.h
+++ Encapsulation/shape.h
@@ -1,1 +1,19 @@
+#ifndef SHAPE_H
+#define SHAPE_H
 
+#include <stdint.h>
+
+
+/* shape's attributes*/
+typedef struct {
+int x;  //x-coordinate of shape's position
+int y;  //y-coordinate of shape's position
+}Shape;
+
+/* shape's operations or methods */
+void shape_ctor(Shape *const me , int x0 , int y0 )            ;
+void shape_moveBy(shape *const me , int dx , int dy)           ;
+int shape_distanceFrom(shape *const me , shape *const other)   ;
+
+
+#endif
