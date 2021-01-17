696142be9d0de9279a9691e602b46cb69d721cc9
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2021-01-17 08:59:46 +0200

    Update main.c

--- Encapsulation/main.c
+++ Encapsulation/main.c
@@ -1,1 +1,22 @@
+#include<shape.h>
 
+Shape s1 ; // static allocation
+
+int main(void)
+{
+  Shape s2 ; // automatic allocation
+  Shape *ps3 = malloc(sizeof(Shape)); // dynamic allocation 
+  
+  /*  act as Constructor...*/
+  shape_ctor(&s1,1,2);
+  shape_ctor(&s2,3,4);
+  shape_ctor(s3,5,6) ;
+  
+  shape_moveBy(&s1,7,8)    ;
+  shape_moveBy(&s2,15,12)  ;
+  shape_moveBy(s3,-1,5)    ;
+  
+  
+  
+ return 0 ; 
+}
