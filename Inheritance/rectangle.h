920df823eb63b57cc3b67ec98ac6f7aa9d0fe7ea
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2021-01-17 09:24:55 +0200

    Update rectangle.h

--- Inheritance/rectangle.h
+++ Inheritance/rectangle.h
@@ -1,1 +1,24 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include<shape.h>
+
+/* Rectangle's attribute..*/
+typedef struct {
+  Shape super  ;   //inherited shape 
+    
+    /* attributes specific to this class*/
+    int width  ;
+    int height ;
+}Rectangle;
+
+/* Rectangle's operations...*/
+void Rectangle_ctor(Rectangle *const me , int x0 , int y0 , int w0 , int h0 );
+void Rectangle_draw(Rectangle const * const me )  ;
+int  Rectangle_area(Rectangle const * const me)   ;
+
+
+#endif
+
+
 
