7f94081a63b34a3534b247e934fa2eeab2b08471
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2021-01-17 08:52:14 +0200

    Update shape.c

--- Encapsulation/shape.c
+++ Encapsulation/shape.c
@@ -1,1 +1,21 @@
-
+void shape_ctor(Shape *const me , int x0 , int y0 )      
+{
+  me->x = x0 ;
+  me->y = y0 ;
+}
+void shape_moveBy(shape *const me , int dx , int dy)           
+{
+  me->x += dx ;
+  me->y += dy ;
+}
+int shape_distanceFrom(shape *const me , shape *const other)
+{
+ int dx = me->x - other->x ;
+ int dy = me->y - other->y ;
+  if(dx < 0 )
+      dx = -dx ;
+  if(dy < 0 )
+    dy = - dy  ;
+  
+  return dx+dy ;
+}
